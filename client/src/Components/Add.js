00e603506b22c6bac185402c641dce0ae49c81bf
Jessica <jhidalgo094@gmail.com>
2020-03-13 10:12:28 -0600

    clean code

--- client/src/Components/Add.js
+++ client/src/Components/Add.js
@@ -1,26 +1,26 @@
 import React from "react";
 import DatePicker from "react-datepicker";
 import "react-datepicker/dist/react-datepicker.css";
-import axios from 'axios';
+import axios from "axios";
 // CSS Modules, react-datepicker-cssmodules.css
 // import 'react-datepicker/dist/react-datepicker-cssmodules.css';
 
 class Add extends React.Component {
-  constructor(props){
-    super(props)
+  constructor(props) {
+    super(props);
     this.state = {
       description: "",
       startDate: new Date(),
       endDate: new Date()
-    }
+    };
   }
 
-  onChange= (e)=>{
-    const {name, value}= e.target;
+  onChange = e => {
+    const { name, value } = e.target;
     this.setState({
       [name]: value
     });
-  }
+  };
 
   handleChange = date => {
     this.setState({
@@ -33,28 +33,31 @@ class Add extends React.Component {
       endDate: date
     });
   };
-  onSubmit = (e)  =>{
+  onSubmit = e => {
     e.preventDefault();
-    const {task} = this.state;
-    axios.post('http://localhost:9000/employee/create',{id: new Date().valueOf().toString(),task})
-  }
-
+    const { task } = this.state;
+    axios.post("http://localhost:9000/employee/create", {
+      id: new Date().valueOf().toString(),
+      task
+    });
+  };
 
   render() {
-
     const { description, startDate, endDate } = this.state;
     return (
       <>
-         
         <td>
-          
           <label>Description</label>
           <div>
-            <input onChange={this.onChange} value={description} name="description"></input>
+            <input
+              onChange={this.onChange}
+              value={description}
+              name="description"
+            ></input>
           </div>
         </td>
-           <td>
-            <label>Start Date</label>
+        <td>
+          <label>Start Date</label>
           <div>
             <DatePicker
               selected={this.state.startDate}
@@ -76,7 +79,7 @@ class Add extends React.Component {
           </div>
         </td>
         <td>
-          <button onSubmit={this.onSubmit} >Submit</button>
+          <button onSubmit={this.onSubmit}>Submit</button>
         </td>
       </>
     );
